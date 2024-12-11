1b39a69019ce9a57be978f26560130600ced18de
Austin Smith <austin.smith.ads@gmail.com>
2024-12-11 16:04:46 -0600

    initial github API variables and necessary namespaces

--- QuaverEd.Data.Objects/Class1.cs
+++ QuaverEd.Data.Objects/Class1.cs
@@ -1,7 +1,28 @@
-﻿namespace QuaverEd.Data.Objects
-{
-    public class Class1
-    {
+﻿// namespace QuaverEd.Data.Objects
+// {
+//     public class Class1
+//     {
 
+//     }
+// }
+
+namespace QuaverEd.Data.Objects
+{
+using System;
+using System.Collections.Generic;
+using System.Data;
+using MySql.Data.MySqlClient;
+using System.Net.Http;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+    public class Program
+    {
+        private const string GitHubApiUrl = "https://api.github.com/search/repositories";
+        private const string MySqlConnectionString = "Server=localhost;User ID=root;Password=password;Database=github_repos;";
     }
+
+
+
+    
 }
